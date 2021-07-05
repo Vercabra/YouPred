03c87a274d6fd7e0cfc2d1d57acd5d1906e018b0
Vercabra <theunikumkid@gmail.com>
2021-07-05 13:56:17 +0300

    adding new graph-building script

--- data_collect.py
+++ data_collect.py
@@ -1,28 +1,26 @@
 from googleapiclient.discovery import build
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from operator import itemgetter
+import matplotlib.font_manager
 
 api_key = 'AIzaSyCZTpGs9kxHFjzrjZnrnafbn5Yww91A5s4'  # our token
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-
 request = youtube.search().list(  # request method that collect data
     part='snippet',  # part of returned dict
     q='Planets',  # search parameter
     maxResults='50',
-    order='date'
+    order='relevance'
 )
 
 response = request.execute()  # making from request response dict
 search_id = []  # video id, needed to get video statistics
 
-
 for j in range(50):  # to get each video search result's id
     search_res_id = response['items'][j]['id']
     search_id.append(search_res_id.get('videoId'))
-
 
 full_video_response = []  # the results with statistics
 for i in search_id:
@@ -32,18 +30,20 @@ for i in search_id:
             id=str(i)
         )
         i_response = request.execute()
-        full_video_response.append((i_response['items'][0]['snippet']['title'],
+        full_video_response.append((int(i_response['items'][0]['snippet']['publishedAt'][0:4] +
+                                        i_response['items'][0]['snippet']['publishedAt'][5:7] +
+                                        i_response['items'][0]['snippet']['publishedAt'][8:10]),
+                                    i_response['items'][0]['snippet']['title'],
                                     i_response['items'][0]['snippet']['publishedAt'],
                                     int((i_response['items'][0]['statistics']['viewCount']))))  # collecting only needed
 
+full_video_response.sort(key=itemgetter(0))
 dates = []
 views = []
 for i in full_video_response:
-    dates.append(i[1][:10])
-    views.append(i[2])
-
+    dates.append(i[2][:10])
+    views.append(i[3])
 
 view_sr = pd.Series(views, dates)
 view_sr.plot()
 plt.show()
-print(view_sr)
