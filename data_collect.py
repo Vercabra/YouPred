184fa73826083e10109fe42f5c77b6d09c954cd8
Vercabra <theunikumkid@gmail.com>
2021-07-27 13:26:07 +0300

    Added unlimited video search (only limited by quota). Reduced time to execute.

--- data_collect.py
+++ data_collect.py
@@ -1,81 +1,50 @@
 from googleapiclient.discovery import build
-import pandas as pd
 from operator import itemgetter
-import plotly.express as px
-import plotly.graph_objects as go
-
+import math
 
 api_key = 'AIzaSyCZTpGs9kxHFjzrjZnrnafbn5Yww91A5s4'  # our token
-
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-srh_request = ''
-request = youtube.search().list(  # request method that collect data
-    part='snippet',  # part of returned dict
-    q=srh_request,  # search request field
-    maxResults='50',
-    order='viewCount'
-)
 
-response = request.execute()  # making from request response dict
-search_id = []  # video id, needed to get video statistics
-
-for j in range(50):  # to get each video search result's id
-    search_res_id = response['items'][j]['id']
-    search_id.append(search_res_id.get('videoId'))
-
-full_video_response = []  # the results with statistics
-for i in search_id:
-    if type(i) is str:  # getting results by video_id
-        request = youtube.videos().list(
-            part='snippet,contentDetails,statistics',
-            id=str(i)
+# VIEW API DOC BEFORE USE THIS FUNCTION!
+# 'q' - search string, 'r_a' - amount of videos, 'o' - sorting order
+def get_search_stat(question: str, res_amount: int, order: str):
+    # collect videoId to get detailed information by video
+    video_id = []  # contains video ids we will use later
+    next_token = ''  # needed to collect next page data
+    page_am = math.ceil(res_amount / 50)  # how many page we are parsing
+    for i in range(page_am):
+        request = youtube.search().list(  # request method that collect data
+            part='snippet',  # part of returned dict
+            q=question,  # search request field
+            maxResults=res_amount - 50 * (page_am - (i + 1)),  # how many results per page
+            order=order,
+            pageToken=next_token
         )
-        i_response = request.execute()
-        full_video_response.append((int(i_response['items'][0]['snippet']['publishedAt'][0:4] +
-                                        i_response['items'][0]['snippet']['publishedAt'][5:7] +
-                                        i_response['items'][0]['snippet']['publishedAt'][8:10]),
-                                    i_response['items'][0]['snippet']['title'],
-                                    i_response['items'][0]['snippet']['publishedAt'],
-                                    int((i_response['items'][0]['statistics']['viewCount']))))  # collecting only needed
+        response = request.execute()
+        for j in range(len(response['items'])):  # collecting Ids
+            video_id.append(response['items'][j]['id']['videoId'])
+        next_token = response['nextPageToken']  # get next page token
 
-full_video_response.sort(key=itemgetter(0))  # sorting by date
-dates = []  # x
-views = []  # y
-for i in full_video_response:  # appending x and y coordinates
-    dates.append(i[2][:10])
-    views.append(i[3])
+    full_response = []
+    for v_id in video_id:
+        v_request = youtube.videos().list(
+            part='snippet,contentDetails,statistics',
+            id=v_id
+        )
+        i_response = v_request.execute()
+        full_response.append((i_response['items'][0]['snippet']['publishedAt'],
+                              int((i_response['items'][0]['statistics']['viewCount']))))
 
-fig = go.Figure()
-
-
-fig.add_trace(go.Scatter(x=dates,
-                         y=views,
-                         line=dict(color='rgb(94, 114, 228)',
-                                   width=3))),
-
-fig.update_layout(width=980,
-                  height=483,
-                  plot_bgcolor='#172b4d',
-                  paper_bgcolor="#172b4d",
-                  xaxis_showgrid=False,
-                  yaxis_showgrid=False,
-                  xaxis_zeroline=False,
-                  yaxis_zeroline=False,
-                  xaxis_anchor='free',
-                  yaxis_anchor='free',
-                  xaxis_position=0,
-                  yaxis_position=0,
-                  title="Total views stats",
-                  title_font_color='#ffffff',
-                  title_font_family='Rubik',
-                  font=dict(
-                      family="Rubik Light",
-                      size=18,
-                      color="#adb5bd"
-                  ))
+    full_response.sort(key=itemgetter(0))
+    X_Y = []
+    for c in full_response:
+        X_Y.append((c[0], c[1]))
+    return X_Y
 
 
-print(fig.to_html(include_plotlyjs='cdn'))
+for i in get_search_stat('Guitar', 100, 'viewCount'):
+    print(i)
+
 
 
